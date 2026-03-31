c434140b30cf4fca592945588d83c64ffcc95aa8
d353rt0pr0d <desertoprod@hotmail.com>
2026-03-31 15:38:06 +0200

    Caricamento automatico 2026-03-31 15:38:06

--- upload.ps1
+++ upload.ps1
@@ -1,3 +1,7 @@
+# Sincronizza prima di caricare
+git pull origin main --rebase
+
+# Aggiungi, committa e spingi
 git add .
 git commit -m "Caricamento automatico $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
 git push origin main
